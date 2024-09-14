f93a40491694f1d13994e31d2ad78cade72cfbc8
karimamnr <karimamnr130@gmail.com>
2024-09-14 11:48:04 +0200

    counterJS

---
+++ index.js
@@ -0,0 +1,18 @@
+const decrease = document.getElementById(`decrease`);
+const reset = document.getElementById(`reset`);
+const increase = document.getElementById(`increase`);
+const label = document.getElementById(`LabelText`);
+ let count=0;
+
+decrease.onclick=function(){
+   count--;
+    label.textContent=count;
+}
+increase.onclick=function(){
+    count++;
+    label.textContent=count;
+}
+reset.onclick=function(){
+    count=0;
+    label.textContent=count;
+}  
