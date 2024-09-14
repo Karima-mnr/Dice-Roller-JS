73894fb9aeeeb3c640de7152a9b5f4490cd6eed0
karimamnr <karimamnr130@gmail.com>
2024-09-14 11:54:21 +0200

    temperature

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-const decrease = document.getElementById(`decrease`);
-const reset = document.getElementById(`reset`);
-const increase = document.getElementById(`increase`);
-const label = document.getElementById(`LabelText`);
- let count=0;
+const textBox = document.getElementById(`textBox`);
+const fah = document.getElementById(`fah`);
+const cel = document.getElementById(`cel`);
+const result = document.getElementById(`result`);
+let temp ;
 
-decrease.onclick=function(){
-   count--;
-    label.textContent=count;
+function convert(){
+    if(fah.checked){
+        temp = Number(textBox.value);
+        temp = (temp * 9/5 ) + (32) ;
+        result.textContent =  temp.toFixed(1) + "°F" ;
+    }
+    else if(cel.checked){
+        temp = Number(textBox.value);
+        temp = (5/9) * (temp - 32);
+        result.textContent = temp.toFixed(1) + "°C";
+    }
+    else{
+        result.textContent = 'Select a unit ';
+    }
 }
-increase.onclick=function(){
-    count++;
-    label.textContent=count;
-}
-reset.onclick=function(){
-    count=0;
-    label.textContent=count;
-}  
